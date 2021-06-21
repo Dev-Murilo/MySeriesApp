26aa1d64d410a8cc58658e6e58f120bc4475343d
Dev-Murilo <silvamurilo689@gmail.com>
2021-06-21 18:11:46 -0300

    classes e modelos

---
+++ Classes/Serie.cs
@@ -0,0 +1,62 @@
+﻿using MySeries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySeries
+{
+    class Serie : Identificador
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int AnoLanc { get; set; }
+        public bool Assistido { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string nome, string descricao, int AnoLanc)
+        {
+            this.Id = id;
+            this.Titulo = nome;
+            this.Descricao = descricao;
+            this.AnoLanc = AnoLanc;
+            this.Assistido = false;
+            this.Excluido = false;
+
+
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano: " + this.AnoLanc + Environment.NewLine;
+
+            return retorno;
+        }
+
+
+
+        public int RetornaId()
+        {
+            return this.Id;
+        }
+        public string RetornaTitulo()
+        {
+            return this.Titulo;
+        }
+        public bool FoiExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+    }
+}
---
+++ Interfaces/IRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySeries.Interface
+{
+    interface IRepo<T>
+    {
+        List<T> Listar();
+        void Inserir(T objeto);
+        void Atualizar(int Id, T objeto);
+        void MarcarAssistido(int Id);
+        void DesmarcarAssistido(int Id);
+        T RetornaPorId(int Id);
+        void Excluir(int Id);
+        int ProximoId();
+
+
+    }
+}
